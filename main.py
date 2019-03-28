988e9a8eacf9230660eaa8a1a6d76e2ff1155176
Shunji Lin <lgxshunji@gmail.com>
2019-03-28 15:24:21 +0900

    add regularization to loss

--- main.py
+++ main.py
@@ -106,7 +106,9 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     labels = tf.reshape(correct_label, (-1, num_classes))
 
     cross_entropy_loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels) +
+        tf.losses.get_regularization_loss()
+    )
     train_op = tf.train.AdamOptimizer(learning_rate).minimize(
         cross_entropy_loss)
     return logits, train_op, cross_entropy_loss
